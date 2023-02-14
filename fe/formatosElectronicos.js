c673f05dd322caed4d8beac163c19fff69679888
benjaminviloria <benjamin.viloria@kodeit.com.mx>
2023-02-14 02:05:18 -0600

    arreglo de pintado de estilos

--- fe/formatosElectronicos.js
+++ fe/formatosElectronicos.js
@@ -8951,19 +8951,16 @@
                 });
             }
         }
-        function insertRule(selector, rul, contxt) {
-            var context = contxt || document, stylesheet;
-
-            if (typeof context.styleSheets === "object") {
-                if (!context.styleSheets.length) {
-                    if (context.createStyleSheet) stylesheet = context.createStyleSheet();
-                    else context.getElementsByTagName("head")[0].appendChild(context.createElement("style"));
-                }
-                stylesheet = context.styleSheets[context.styleSheets.length - 1];
-                if (stylesheet) {
-                    if (stylesheet.addRule) for (var i = 0; i < selector.length; ++i) stylesheet.addRule(selector[i], rul);
-                    else stylesheet.insertRule(selector.join(",") + "{" + rul + "}", stylesheet.cssRules.length);
-                }
+        function insertRule(selector, rul) {
+            if (!$('#dynamicstyle').length) {
+                var dynsty = document.createElement("style");
+                dynsty.id = "dynamicstyle";
+                document.getElementsByTagName("head")[0].appendChild(dynsty);
+            }
+            var stylesheet = $('#dynamicstyle')[0].sheet;
+            if (stylesheet) {
+                if (stylesheet.addRule) for (var i = 0; i < selector.length; ++i) stylesheet.addRule(selector[i], rul);
+                else stylesheet.insertRule(selector.join(",") + "{" + rul + "}", stylesheet.cssRules.length);
             }
         }
         function importfe() {
